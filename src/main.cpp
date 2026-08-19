467004d0d2d3988d6454505b280599caf951bee8
karanchaudhary-111 <karanchaudhary779m@gmail.com>
2026-08-19 20:37:18 +0530

    Support multiple equipments

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include "Equipment.h"
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    Equipment motor("Motor-01", 70.0, 5.2, 2.5, "NORMAL");
+    vector<Equipment> equipmentList;
 
-    motor.display();
+    equipmentList.push_back(
+        Equipment("Motor-01", 70.0, 5.2, 2.5, "NORMAL")
+    );
+
+    equipmentList.push_back(
+        Equipment("Pump-01", 82.0, 6.1, 4.2, "WARNING")
+    );
+
+    for (Equipment equipment : equipmentList)
+    {
+        equipment.display();
+        cout << "----------------------" << endl;
+    }
 
     return 0;
 }
